6d757a84bcf57ab9ea73333aba5b2ca0ffb12f5c
Maria Luiza Ratto <marialuizamratto@gmail.com>
2025-06-17 15:58:32 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,19 +20,23 @@ def carregar_dados():
 df = carregar_dados()
 
 perguntas = {
-    "345311-270": "Você concorda com o [Marco Temporal](https://www.camara.leg.br/noticias/966618-o-que-e-marco-temporal-e-quais-os-argumentos-favoraveis-e-contrarios/#:~:text=Marco%20temporal%20%C3%A9%20uma%20tese,data%20de%20promulga%C3%A7%C3%A3o%20da%20Constitui%C3%A7%C3%A3o.) para demarcação de terras indígenas?",
-    "2438467-47": "Você apoia a criação do Dia Nacional para a Ação Climática?",
-    "2207613-167": "Você é contra a privatização de empresas de saneamento básico?",
-    "264726-144": "Você apoia o aumento de pena para porte ilegal de arma?",
-    "604557-205": "Você apoia a [Lei do Mar](https://www.camara.leg.br/noticias/1163592-camara-aprova-projeto-que-cria-a-lei-do-mar), que regula a exploração sustentável dos recursos marítimos?",
-    "2417025-55": "Você concorda que uma pessoa que ganha 2 salários mínimos deve pagar imposto de renda?",
-    "2231632-97": "Você concorda que documentos públicos devem usar linguagem acessível?",
-    "2345281-63": "Você concorda que mulheres têm direito à cirurgia reparadora das mamas após câncer pelo SUS?",
-    "2078693-87": "Você apoia repasses federais mesmo para municípios inadimplentes, se for para combater a violência contra a mulher?",
-    "2310025-56": "Você apoia a [Lei Aldir Blanc](https://www.gov.br/pt-br/noticias/cultura-artes-historia-e-esportes/2020/08/lei-aldir-blanc-de-apoio-a-cultura-e-regulamentada-pelo-governo-federal) de incentivo à cultura?",
-    "2266116-87-1": "Você concorda que presos por homicídio qualificado devem cumprir pena em estabelecimentos penais federais de segurança máxima?",
-    "2266116-87-2": "Você concorda que presos por homicídio qualificado devem cumprir Regime Disciplinar Diferenciado em unidades federais?"
+{
+    "345311-270": "O marco temporal deve ser adotado como critério para a demarcação de terras indígenas.",
+    "2438467-47": "O Brasil deve instituir o Dia Nacional para a Ação Climática.",
+    "2207613-167": "Sou a favor da privatização de empresas de saneamento básico.",
+    "264726-144": "A pena para porte ilegal de arma deve ser aumentada.",
+    "604557-205": "A Lei do Mar, que regula a exploração sustentável dos recursos marítimos, deve ser aprovada.",
+    "2417025-55": "Uma pessoa que ganha 2 salários mínimos deve pagar imposto de renda.",
+    "2231632-97": "Documentos públicos devem obrigatoriamente usar linguagem acessível.",
+    "2345281-63": "Mulheres têm direito à cirurgia reparadora das mamas pelo SUS após o câncer.",
+    "2078693-87": "O governo federal deve repassar recursos a municípios inadimplentes para combater a violência contra a mulher.",
+    "2310025-56": "A Lei Aldir Blanc de incentivo à cultura deve ser apoiada.",
+    "2266116-87-1": "Presos por homicídio qualificado devem cumprir pena em estabelecimentos penais federais de segurança máxima.",
+    "2266116-87-2": "Presos por homicídio qualificado devem cumprir Regime Disciplinar Diferenciado em unidades federais.",
+    "2171314-10": "É urgente discutir e aprovar um projeto de lei sobre fake news.",
+    "2220292-229": "O voto impresso deve substituir o voto por urna eletrônica."
 }
+
 
 pesos_usuario = {
     "Discordo muito": -2,
