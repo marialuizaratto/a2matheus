9d3292db4ea870d4c9cd47727aa531cd73d6b42e
Maria Luiza Ratto <marialuizamratto@gmail.com>
2025-06-17 14:50:56 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,17 +19,18 @@ def carregar_dados():
 df = carregar_dados()
 
 perguntas = {
-    "345311-270": "Você concorda com o [Marco Temporal](https://www.camara.leg.br/noticias/966618-o-que-e-marco-temporal-e-quais-os-argumentos-favoraveis-e-contrarios/#:~:text=Marco%20temporal%20%C3%A9%20uma%20tese,data%20de%20promulga%C3%A7%C3%A3o%20da%20Constitui%C3%A7%C3%A3o.) para demarcação de terras indígenas?",
+    "345311-270": 'Você concorda com o <a href="https://www.camara.leg.br/noticias/966618-o-que-e-marco-temporal-e-quais-os-argumentos-favoraveis-e-contrarios/#:~:text=Marco%20temporal%20%C3%A9%20uma%20tese,data%20de%20promulga%C3%A7%C3%A3o%20da%20Constitui%C3%A7%C3%A3o." target="_blank" style="color:blue; text-decoration:underline;">Marco Temporal</a> para demarcação de terras indígenas?',
     "2438467-47": "Você apoia a criação do Dia Nacional para a Ação Climática?",
     "2207613-167": "Você é contra a privatização de empresas e o aumento de custos no saneamento básico?",
     "264726-144": "Você apoia o aumento de pena para porte ilegal de arma?",
-    "604557-205": "Você apoia a [Lei do Mar](https://www.camara.leg.br/noticias/1163592-camara-aprova-projeto-que-cria-a-lei-do-mar), que regula a exploração sustentável dos recursos marítimos?",
+    "604557-205": 'Você apoia a <a href="https://www.camara.leg.br/noticias/1163592-camara-aprova-projeto-que-cria-a-lei-do-mar" target="_blank" style="color:blue; text-decoration:underline;">Lei do Mar</a>, que regula a exploração sustentável dos recursos marítimos?',
     "2417025-55": "Você concorda que uma pessoa que ganha 2 salários mínimos deve pagar imposto de renda?",
     "2231632-97": "Você concorda que documentos públicos devem usar linguagem acessível?",
     "2345281-63": "Você concorda que mulheres têm direito à cirurgia reparadora das mamas após câncer pelo SUS?",
     "2078693-87": "Você apoia repasses federais mesmo para municípios inadimplentes, se for para combater a violência contra a mulher?",
-    "2310025-56": "Você apoia a [Lei Aldir Blanc](https://www.gov.br/pt-br/noticias/cultura-artes-historia-e-esportes/2020/08/lei-aldir-blanc-de-apoio-a-cultura-e-regulamentada-pelo-governo-federal) de incentivo à cultura?"
+    "2310025-56": 'Você apoia a <a href="https://www.gov.br/pt-br/noticias/cultura-artes-historia-e-esportes/2020/08/lei-aldir-blanc-de-apoio-a-cultura-e-regulamentada-pelo-governo-federal" target="_blank" style="color:blue; text-decoration:underline;">Lei Aldir Blanc</a> de incentivo à cultura?'
 }
+
 
 pesos_usuario = {
     "Discordo muito": -2,
