8b84a1e65d70d5a6132c5e111a06df9b22a386ee
Maria Luiza Ratto <marialuizamratto@gmail.com>
2025-06-18 01:12:55 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -20,10 +20,9 @@ def carregar_dados():
 df = carregar_dados()
 
 perguntas = {
-
     "345311-270": "O marco temporal deve ser adotado como critério para a demarcação de terras indígenas.",
     "2438467-47": "O Brasil deve instituir o Dia Nacional para a Ação Climática.",
-    "2207613-167":"Sou a favor da privatização de empresas de saneamento básico.",
+    "2207613-167": "Sou a favor da privatização de empresas de saneamento básico.",
     "264726-144": "A pena para porte ilegal de arma deve ser aumentada.",
     "604557-205": "A Lei do Mar, que regula a exploração sustentável dos recursos marítimos, deve ser aprovada.",
     "2417025-55": "Uma pessoa que ganha 2 salários mínimos deve pagar imposto de renda.",
@@ -31,11 +30,11 @@ perguntas = {
     "2345281-63": "Mulheres têm direito à cirurgia reparadora das mamas pelo SUS após o câncer.",
     "2078693-87": "O governo federal deve repassar recursos a municípios inadimplentes para combater a violência contra a mulher.",
     "2310025-56": "A Lei Aldir Blanc de incentivo à cultura deve ser apoiada.",
-    "2266116-87-1": "Presos por homicídio qualificado devem cumprir pena em estabelecimentos penais federais de segurança máxima.",
-    "2266116-87-2": "Presos por homicídio qualificado devem cumprir Regime Disciplinar Diferenciado em unidades federais.",
+    "2266116-87": "Presos por homicídio qualificado devem cumprir pena em estabelecimentos federais sob regime disciplinar diferenciado.",
     "2171314-10": "É urgente discutir e aprovar um projeto de lei sobre fake news.",
     "2220292-229": "O voto impresso deve substituir o voto por urna eletrônica."
 }
+
 
 
 pesos_usuario = {
